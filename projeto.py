98525fb21bc8e47673e88d8a06976b9146c053ab
antonioogo <agomesquadro@gmail.com>
2022-10-07 21:58:29 -0300

    quase la

--- projeto.py
+++ projeto.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request
 import requests, mysql.connector
 from requests.structures import CaseInsensitiveDict
 
@@ -8,44 +8,70 @@ app = Flask(__name__)
 def webhook():
     dicionario = request.get_json()
 
-    #Coleto dados da mensagem
-    strUpdateId  = str(dicionario['result'][0]['update_id'])
+    #Coleta dados da mensagem
     strMensagem  = str(dicionario['result'][0]['message']['text'])
     strNome      = str(dicionario['result'][0]['message']['from']['first_name'])
     strChatId    = str(dicionario['result'][0]['message']['from']['id'])
 
     #Verifica se é a primeira mensagem
-    bPrimeiraMensagem = verificaSePrimeiraMensagem(strChatId)
+    bPrimeiraMensagem = True
+    tabFluxoAtual     = retornaFluxoAtual(strChatId)
+
+    if len(tabFluxoAtual) > 0:
+        bPrimeiraMensagem = False
+        strFluxoAtual     = str(tabFluxoAtual[0][0])
+        strSequenciaAtual = str(tabFluxoAtual[0][1])
 
     #Verifica se está cadastrado
-    bExisteCadastro = verificaCadastro(strChatId)
+    #bExisteCadastro = verificaCadastro(strChatId)
 
     if bPrimeiraMensagem:
         entraFluxoConversa(strChatId, "1")
+        strFluxoAtual     = 1
+        strSequenciaAtual = 1
 
-    #Guarda a mensagem no BD
-    salvaMensagem(strChatId, strMensagem, strNome)
+        #Guarda a mensagem no BD
+        salvaMensagem(strChatId, strMensagem, strNome)
 
-    #Verifica resposta recebida
-    #funcao que recebe o chat id, ela vai ver a sequencia atual e as respostas aceitas
-    #loop nas respostas aceitas, se uma delas for a mensagem recebida, continua o fluxo
-    #se nao, envia mensagem que nao entendeu
+    else:
+        #Guarda a mensagem no BD
+        salvaMensagem(strChatId, strMensagem, strNome)
+
+        #Verifica resposta recebida
+        #Traz todas as respostas aceitas daquela sequencia/fluxo atual
+        tabRespostas = selectBanco("SELECT RESPACEITA, IDFLUXOREDIREC FROM RESPOSTAFLUXO WHERE IDFLUXO = '" + strFluxoAtual + "' AND NUMSEQ = '" + strSequenciaAtual + "';")
+        bRespostaAceita = False
+
+        #Loop nas respostas aceitas verificando se ela é igual a resposta recebida
+        if len(tabRespostas) > 0:
+            for linha in tabRespostas:
+                strResposta = linha[0]
+
+                if strResposta == '*':
+                    bRespostaAceita = True
+                    break
+
+                elif strMensagem.upper() == strResposta.upper():
+                    bRespostaAceita = True
+                    break
 
 
-    continuaFluxo(strChatId)
+        if bRespostaAceita:
+            #Responde e continua o fluxo
+            continuaFluxo(strChatId)
+        elif len(tabRespostas) == 0:
+            entraFluxoConversa(strChatId, "1")
+        else:
+            enviaMsg(strChatId, 'Nao entendi sua resposta, por favor responda corretamente: ')
 
-    return "ok"
+        #IDENTIFICAR QUANDO ACABAR O FLUXO
+        #MELHORAR A FUNÇÃO entraFluxoConversa PARA RESPONDER
+        #TALVEZ UNIFICAR A FUNÇÃO CONTINUA COM A ENTRA
+        #TA DEMORANDO 2 SEGUNDOS PRA RESPONDER, MELHORAR ISSO
+
+    return str(strSequenciaAtual)
 
 #Funções de fluxo
-
-def verificaSePrimeiraMensagem(strChatId):
-    bPrimeiraMensagem = True
-    tabBancoDados = selectBanco("SELECT * FROM CONTATOS WHERE DATAHORA >= CURRENT_TIMESTAMP()-10000 AND IDCTT = " + str(strChatId) + ";")
-
-    if len(tabBancoDados) > 0:
-        bPrimeiraMensagem = False
-    
-    return bPrimeiraMensagem
 
 def verificaCadastro(strChatId):
     bExisteCadastro = False
@@ -57,12 +83,12 @@ def verificaCadastro(strChatId):
     
     return bExisteCadastro
 
-def salvaMensagem(strIDCTT, strMensagem, strNomeCtt):
+def salvaMensagem(strChatId, strMensagem, strNomeCtt):
     con = mysql.connector.connect(host='botuni9.c3cupjqiyqbn.sa-east-1.rds.amazonaws.com', database='ChatBot', user='admin', password='7pPdu#GSX.2sYG')
 
     if con.is_connected():
         cursor = con.cursor()
-        cursor.execute("INSERT INTO MENSAGENS_RECEBIDAS (IDCTT, MENSAGEM, NOMECTT) VALUES ('" + strIDCTT + "', '" + strMensagem + "', '" + strNomeCtt + "');")
+        cursor.execute("INSERT INTO MENSAGENS_RECEBIDAS (IDCTT, MENSAGEM, NOMECTT) VALUES ('" + strChatId + "', '" + strMensagem + "', '" + strNomeCtt + "');")
         con.commit()
 
         cursor.close()
@@ -70,43 +96,50 @@ def salvaMensagem(strIDCTT, strMensagem, strNomeCtt):
 
         return "ok"
 
-def entraFluxoConversa(strIDCTT, strIDFluxo):
+def entraFluxoConversa(strChatId, strIDFluxo):
     #registrar na tabela contatos o fluxo
-    insertUpdateDeleteBanco("DELETE FROM CONTATOS WHERE IDCTT = '" + strIDCTT + "';")
-    insertUpdateDeleteBanco("INSERT INTO CONTATOS (IDCTT, IDFLUXOATUAL, IDNUMSEQATUAL) VALUES ('" + strIDCTT + "', " + strIDFluxo + ", 1);")
+    insertUpdateDeleteBanco("DELETE FROM CONTATOS WHERE IDCTT = '" + strChatId + "';")
+    insertUpdateDeleteBanco("INSERT INTO CONTATOS (IDCTT, IDFLUXOATUAL, IDNUMSEQATUAL) VALUES ('" + strChatId + "', " + strIDFluxo + ", 1);")
+    tabBancoDados = selectBanco("SELECT MENSAGEM FROM FLUXOSMSG WHERE IDFLUXO = '" + strIDFluxo + "' AND NUMSEQ = '1';")
+
+    enviaMsg(strChatId, str(tabBancoDados[0][0]))
 
     return "ok"
 
-def continuaFluxo(strIDCTT):
+def continuaFluxo(strChatId):
     #Verifica o fluxo atual
-    tabBancoDados      = selectBanco("SELECT IDFLUXOATUAL, IDNUMSEQATUAL FROM CONTATOS WHERE IDCTT = '" + strIDCTT + "';")
+    tabBancoDados = retornaFluxoAtual(strChatId)
     strIDFluxo = str(tabBancoDados[0][0])
-    strNumSeq  = str(tabBancoDados[0][1])
+    strNumSeq  = str(int(tabBancoDados[0][1]) + 1)
 
     #Busca a mensagem da sequencia atual
     tabBancoDadosFluxo = selectBanco("SELECT MENSAGEM FROM FLUXOSMSG WHERE IDFLUXO = '" + strIDFluxo + "' AND NUMSEQ = '" + strNumSeq +  "';")
 
-    #atualiza para a proxima mensagem
-    insertUpdateDeleteBanco("UPDATE CONTATOS SET IDNUMSEQATUAL = '" + str((int(strNumSeq) + 1)) + "' WHERE IDCTT = '" + strIDCTT + "'")
-    enviaMsg(strIDCTT, str(tabBancoDadosFluxo[0][0]))
-
-
+    if len(tabBancoDadosFluxo) > 0:
+        #atualiza para a proxima mensagem
+        insertUpdateDeleteBanco("UPDATE CONTATOS SET IDNUMSEQATUAL = '" + strNumSeq + "' WHERE IDCTT = '" + strChatId + "'")
+        enviaMsg(strChatId, str(tabBancoDadosFluxo[0][0]))
+    else:
+        entraFluxoConversa(strChatId, "1")
+        
     return "ok"
 
-    
+def retornaFluxoAtual(strChatId):
+    tabBancoDados = selectBanco("SELECT IDFLUXOATUAL, IDNUMSEQATUAL FROM CONTATOS WHERE IDCTT = '" + strChatId + "';")
 
-def enviaMsg(strIDCTT, strMensagem):
+    return tabBancoDados
+
+def enviaMsg(strChatId, strMensagem):
 
     url = "https://api.telegram.org/bot5751250760:AAG6Fs7zjgKKG8u9S_1BkO53Tn6z5u5C4XI/sendMessage"
 
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
-    data = '{"chat_id":"' + strIDCTT + '","text":"' + strMensagem + '"}'
+    data = '{"chat_id":"' + strChatId + '","text":"' + strMensagem + '"}'
 
     requests.post(url, headers=headers, data=data)
 
     return "ok"
-
 
 #Funções de Banco de Dados
 
