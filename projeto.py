5239e35ec3d3dd99e2e0d44acf4c0ef22b5e8311
antonioogo <agomesquadro@gmail.com>
2022-10-07 22:26:09 -0300

    quase la 4

--- projeto.py
+++ projeto.py
@@ -12,10 +12,13 @@ def webhook2():
 
 @app.route('/webhook', methods = ['POST'])
 def webhook():
+    #insertUpdateDeleteBanco("INSERT INTO LOG (RETORNO, ETAPA) VALUES ('" +  + "', '1')")
+    insertUpdateDeleteBanco("INSERT INTO LOG (RETORNO, ETAPA) VALUES ('INCICIO', '1')")
     dicionario = request.get_json()
 
     #Coleta dados da mensagem
     strMensagem  = str(dicionario['result'][0]['message']['text'])
+    insertUpdateDeleteBanco("INSERT INTO LOG (RETORNO, ETAPA) VALUES ('" + strMensagem + "', '1')")
     strNome      = str(dicionario['result'][0]['message']['from']['first_name'])
     strChatId    = str(dicionario['result'][0]['message']['from']['id'])
 
