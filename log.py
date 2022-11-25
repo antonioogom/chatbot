f2fb11f50cb3dae56de7cd7fd0e79680edc95a3d
antonioogo <agomesquadro@gmail.com>
2022-11-24 21:08:25 -0300

    Teste agendador

--- log.py
+++ log.py
@@ -4,7 +4,7 @@ import mysql.connector
 objConexao = mysql.connector.connect(host='botuni9.c3cupjqiyqbn.sa-east-1.rds.amazonaws.com', database='ChatBot', user='admin', password='7pPdu#GSX.2sYG')
 
 cursor = objConexao.cursor()
-cursor.execute("INSERT INTO LOG (RETORNO, ETAPA) VALUES ('Ocorreu um erro na função EnviaMsg', 'Erro');")
+cursor.execute("INSERT INTO LOG (RETORNO, ETAPA) VALUES ('Registro de log', 'Log');")
 objConexao.commit()
 
 cursor.close()
